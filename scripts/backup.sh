0efc2591472a9a21107a2a1c79466c5da9b4b310
jackytsheng <jackytsheng@gmail.com>
2021-08-01 20:24:39 +1000

    add backup file

---
+++ scripts/backup.sh
@@ -0,0 +1,5 @@
+
+echo 'Backing up files...'
+rm -r ../minecraft-backup/minecraft-world/uK1tX4ZiBgA=/*
+cp -r ../minecraft-server/worlds/'Jacky and Wing World' ../minecraft-backup/minecraft-world/uK1tX4ZiBgA=/*
+echo 'Finish back up'
