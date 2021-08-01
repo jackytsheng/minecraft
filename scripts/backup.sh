4150830cdfb70bd4c56834860660d246b54b8ff0
jackytsheng <jackytsheng@gmail.com>
2021-08-01 20:27:18 +1000

    edit script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,5 +1,5 @@
 
 echo 'Backing up files...'
-rm -r ../minecraft-backup/minecraft-world/uK1tX4ZiBgA=/*
-cp -r ../minecraft-server/worlds/'Jacky and Wing World' ../minecraft-backup/minecraft-world/uK1tX4ZiBgA=/*
+rm -r ../minecraft-world/minecraft-world/uK1tX4ZiBgA=/*
+cp -r ../minecraft-server/worlds/'Jacky and Wing World' ../minecraft-world/uK1tX4ZiBgA=/*
 echo 'Finish back up'
--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,5 +1,5 @@
 
 echo 'Copying files to worlds ...'
 rm -r ../minecraft-server/worlds/'Jacky and Wing World'/*
-cp -r ../minecraft-backup/minecraft-world/uK1tX4ZiBgA=/* ../minecraft-server/worlds/'Jacky and Wing World'
+cp -r ../minecraft-world/uK1tX4ZiBgA=/* ../minecraft-server/worlds/'Jacky and Wing World'
 echo 'Finish copying'
