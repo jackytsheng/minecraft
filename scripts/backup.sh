bf1554457c20af63c25d887c3f7126599bb18435
jiajin-zheng <jiajin.zheng@xero.com>
2021-08-02 19:59:52 +1000

    added script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,5 +1,9 @@
+echo "Zipping world"
+zip backup.zip -r ../minecraft-server/worlds/'Jacky and Wing World'
 
-echo 'Backing up files...'
-rm -r ../minecraft-world/uK1tX4ZiBgA=/*
-cp -r ../minecraft-server/worlds/'Jacky and Wing World' ../minecraft-world/uK1tX4ZiBgA=/*
-echo 'Finish back up'
+echo "Uploading to Object Storage"
+
+# Using Default Configuration
+oci os object put --namespace <object_storage_namespace> --bucket-name <bucket_name> --file <file_location> [--name <object_name>] [--storage-tier <object_storage_tier>] --no-multipart
+
+echo "succcessfully upload"
---
+++ scripts/install-mc.sh
@@ -0,0 +1,9 @@
+# Install server
+cd minecraft-server
+
+curl https://minecraft.azureedge.net/bin-linux/bedrock-server-1.17.10.04.zip --output mc.zip
+
+unzip mc.zip
+
+# Delete the zip file once install
+rm mc.zip
